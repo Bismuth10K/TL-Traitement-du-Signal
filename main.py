6f67c32ad60d714b702de3fbcbe5c096e9400087
10K_Bismuth <bismuthdev@gmail.com>
2025-05-20 08:25:59 +0200

    1.2.2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import numpy as np
+from TL import *
 
 samplerate_bj, data_bj = wavfile.read('./audacity/bonjour.wav')
 samplerate_ar, data_ar = wavfile.read('./audacity/aurevoir.wav')
-
+print(data_bj)
 print(samplerate_bj)
 print(samplerate_ar)
 
 fig, axs = plt.subplots(2)
-axs[0].plot(np.arange(len(data_bj)) / samplerate_bj, data_bj)
-axs[1].plot(np.arange(len(data_ar)) / samplerate_ar, data_ar)
+t_bj = np.arange(len(data_bj)) / samplerate_bj
+t_ar = np.arange(len(data_ar)) / samplerate_ar
+axs[0].plot(t_bj, data_bj)
+axs[1].plot(t_ar, data_ar)
 plt.show()
+
+
+#! Q 1.2.3
+new_t_bj, new_y_bj = quantify(t_bj, data_bj, 4)
