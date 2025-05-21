13e3a6fc742f62b7d7d2d721efa2775a90f8dba0
10K_Bismuth <bismuthdev@gmail.com>
2025-05-21 16:41:08 +0200

    Plots à jour

--- TL.py
+++ TL.py
@@ -6,7 +6,10 @@ def gf(f0, fe, n):
     t = np.arange(0, n/fe, 1/fe)
     y = np.sin(2 * np.pi * f0 * t)
     plt.plot(t, y)
-    plt.show()
+    plt.xlabel("Temps (s)")
+    plt.ylabel("Amplitude")
+    # plt.savefig("./plots/q1.1.1.png")
+    # plt.show()
     return t, y
 
 
@@ -14,15 +17,15 @@ def energy(t, y):
     return np.trapezoid(y**2, t)
 
 sig = gf(0.1,10,200)
-# print(len(sig[1]))
-# print(energy(sig[0], sig[1]))
+print(len(sig[1]))
+print(energy(sig[0], sig[1]))
 
 
 def mean_power(t, y, f0, fe):
     stop = int(fe/f0 + 1)
     return np.trapezoid(y[:stop]**2, t[:stop])
 
-# print(mean_power(sig[0], sig[1], 0.1, 10))
+print(mean_power(sig[0], sig[1], 0.1, 10))
 
 
 def quantify(t, y, n):
@@ -35,11 +38,15 @@ def quantify(t, y, n):
         diff = np.abs(pt - tab2)
         new_y.append(tab2[np.argmin(diff)])
     plt.plot(t, new_y)
+    plt.xlabel("Temps (s)")
+    plt.ylabel(f"Signal quantifié sur {n} bits")
+    # Rajouter -2 à la fin du titre pour un n = 3
+    # plt.savefig("./plots/q1.1.3.png")
     plt.show()
     return t, new_y
 
 
-# quantify(sig[0], sig[1], 2)
+quantify(sig[0], sig[1], 8)
 
 def noise_energy(t, n):
     return ((n**2)/12) * (t[-1]-t[0])
